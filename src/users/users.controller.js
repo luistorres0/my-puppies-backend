e411b90527f1dc5a7902e64e412794f838c25c18
Luis Torres <luisdtor@gmail.com>
2022-02-18 15:19:56 -0600

    Added bcrypt for security

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,26 +1,33 @@
 const users = [];
 
-function create(req, res, next) {
-  const { username, password } = req.body.data;
+const bcrypt = require("bcrypt");
+
+async function create(req, res, next) {
+  const { email, password } = req.body.data;
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
 
   const user = {
-    username,
-    password,
+    email,
+    password: hashedPassword,
   };
+
   users.push(user);
+
+  console.log(users);
   res.sendStatus(201);
 }
 
-function authenticate(req, res, next) {
-  const { username, password } = req.body.data;
+async function authenticate(req, res, next) {
+  const { email, password } = req.body.data;
 
-  const foundUser = users.find((user) => user.username === username);
+  const foundUser = users.find((user) => user.email === email);
 
   if (!foundUser) {
     return next({ status: 404, message: "User not found" });
   }
 
-  if (foundUser.password !== password) {
+  if (!await bcrypt.compare(password, foundUser.password)) {
     return next({ status: 400, message: "Invalid password" });
   }
 
